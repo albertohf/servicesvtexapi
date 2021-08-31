44cc6750167fe01a896a0d14e7ac26cbb9aaf885
Alberto Fernandes <albertohf91@gmail.com>
2021-08-30 21:02:41 -0300

    completo

--- node/handlers/oms.ts
+++ node/handlers/oms.ts
@@ -1,4 +1,5 @@
 import json from 'co-body'
+import axios from 'axios'
 
 
 export async function oms(ctx: Context, next: () => Promise<any>) {
@@ -9,13 +10,41 @@ export async function oms(ctx: Context, next: () => Promise<any>) {
   console.log(userId)
 
 
+  const data:any = await ctx.clients.masterdata.searchDocuments({
+    dataEntity:'CL',
+    where:`email=${userId?.split("-",1)}`,
+    fields:['email','firstName','lastName','phone'],
+    pagination:{
+      pageSize:1,
+      page:1
+    }
+  })
+  console.log(data)
 
-  ctx.status = 200
-  ctx.set('Cache-Control', 'no-cache no-store')
 
-  // const data = await oms.order("")
-  ctx.body = "ok"
-  await next()
+  if(!data){
+    throw new Error("MasterData not Found")
+  }
+
+  axios.put(
+    "https://zq7fojw450.execute-api.us-east-2.amazonaws.com/type",{email:data[0].email}
+  ).catch((e)=>{ axios.put("https://zq7fojw450.execute-api.us-east-2.amazonaws.com/leads",{email:data[0].email,name:data[0].firstName +" "+ data[0].lastName,phone:data[0].phone,type:'Client'}) })
+
+
+
+
+  // try{
+  //   const awsPut = await axios.put(
+  //     "https://zq7fojw450.execute-api.us-east-2.amazonaws.com/type",{email:data[0].email}
+  //   )
+  //   return awsPut
+  // }catch(err){
+  //   console.log(err)
+  //   const awsPost = await axios.put(
+  //     "https://zq7fojw450.execute-api.us-east-2.amazonaws.com/leads",{email:data[0].email,name:data[0].firstName +" "+ data[0].lastName,phone:data[0].phone}
+  //   )
+  //   return awsPost
+  // }
 
 
 // key
